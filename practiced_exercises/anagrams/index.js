bf5dbed5658b6e7f633cbe88b4a7e414298a4f0c
Stephan Lefort <lefortsge@gmail.com>
2021-08-13 17:30:48 -0700

    Update index.js

--- practiced_exercises/anagrams/index.js
+++ practiced_exercises/anagrams/index.js
@@ -10,34 +10,43 @@
 
 
 // Solution 1
+// function anagrams(stringA, stringB) {
+//
+//   let aCharMap = buildCharMap(stringA);
+//   let bCharMap = buildCharMap(stringB);
+//
+// // Use Object.keys() to get all the keys of the object and calculate the length
+//   if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+//     return false;
+//   }
+//
+//   for(let char in aCharMap) {
+//     if(aCharMap[char] !== bCharMap[char]) {
+//       return false;
+//     }
+//   }
+//
+//   return true;
+// }
+//
+// function buildCharMap(str) {
+//   const charMap = {};
+//
+//   // Make use of regex expression to eliminate punctuations and white spaces
+//   for (let char of str.replace(/[^\w]/g,"").toLowerCase()) {
+//       charMap[char] = charMap[char] + 1 || 1 ;
+//   }
+//
+//   return charMap;
+// }
+
+// Solution 2
 function anagrams(stringA, stringB) {
+  return cleanStr(stringA) === cleanStr(stringB);
+};
 
-  let aCharMap = buildCharMap(stringA);
-  let bCharMap = buildCharMap(stringB);
-
-// Use Object.keys() to get all the keys of the object and calculate the length
-  if (Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
-    return false;
-  }
-
-  for(let char in aCharMap) {
-    if(aCharMap[char] !== bCharMap[char]) {
-      return false;
-    }
-  }
-
-  return true;
-}
-
-function buildCharMap(str) {
-  const charMap = {};
-
-  // Make use of regex expression to eliminate punctuations and white spaces
-  for (let char of str.replace(/[^\w]/g,"").toLowerCase()) {
-      charMap[char] = charMap[char] + 1 || 1 ;
-  }
-
-  return charMap;
+function cleanStr(str) {
+  return str.replace(/[^\w]/g, '').split('').sort().join('');
 }
 
 module.exports = anagrams;
